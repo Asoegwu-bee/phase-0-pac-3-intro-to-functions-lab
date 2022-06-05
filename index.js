43483258a51d63f308ac5afb4dfd2abef824ca08
Bianca Emmanuel <bianca.emmanuel@student.moringaschool.com>
2022-06-05 10:18:05 +0100

    commit tree ae47cc7f53c42c7ba9d9fdc4ddc0b171e4c4a32f

---
+++ index.js
@@ -0,0 +1,24 @@
+const string = "Hello";
+
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+const logShout = (string);
+console.log(string);
+console.log(string.toUpperCase())
+
+const logWhisper = (string);
+console.log(string);
+console.log(string.toLowerCase())
+
+function sayHiToHeadphonedRoommate(string) {
+
+}
+
+
+
+
